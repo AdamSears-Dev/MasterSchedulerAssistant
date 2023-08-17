71bbb5955db680b1ad1368f54d957fb4af9fd576
Adam Sears <adam_sears031@hotmail.com>
2023-08-17 17:35:09 -0400

    Data Preparation
    Created a format to digitized and structure schedules from csv.
    
    to do:
    
    Establish database connection and code API layer

---
+++ ScheduleDataImport.py
@@ -0,0 +1,12 @@
+#import library
+import pandas as pd
+
+#read csv file
+df = pd.read_csv('C:/Users/Adam/Desktop/Master Scheduler Assistant App/FirstGradeWeek.csv')
+
+#display csv file
+print(df.head())
+
+#to do establish connection to database
+
+#to do code API layer
